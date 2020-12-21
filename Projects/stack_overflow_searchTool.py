2d8e527c5cb97d2a331e0af742edb6b210a1de6d
mpscoder <67371648+mpscoder@users.noreply.github.com>
2020-12-21 23:15:04 +0530

    Add files via upload

---
+++ Projects/stack_overflow_searchTool.py
@@ -0,0 +1,17 @@
+# ************************************************************
+#                 Stack Overflow Search Tool
+#                  Project By : MAMATA SHEE
+# ************************************************************
+
+
+
+#-----------------------------------------------
+# -->for this we have to install a package named "howdoi"
+# -->use the below command to install "howdoi"
+# --> 'pip install howdoi'
+# 
+# --> After installing you can search like below:
+# --> howdoi your_query
+# ------------------------------------------------
+
+
