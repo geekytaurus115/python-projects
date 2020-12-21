49a818b9994bfb4fb12fe818097ed032680afc57
mpscoder <67371648+mpscoder@users.noreply.github.com>
2020-12-21 22:24:48 +0530

    Create copy_paste.py

---
+++ Projects/copy_paste.py
@@ -0,0 +1,12 @@
+# ************************************************************
+#                 copy-paste mini project
+#                 Project By : MAMATA SHEE
+# ************************************************************
+
+
+import pyperclip
+s=input("Enter any string that you want to copy and then paste : ")
+pyperclip.copy(s)
+pyperclip.paste()
+
+
